65d70def84fdeeecc5c818acd25a39cbd7e53501
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-03-01 00:11:01 +0200

    feat: add user register and login joi schemas

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -1,26 +1,22 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const Regexps = { EMAIL: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ };
 
 const registerSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required().messages({
-    "string.pattern.base":
-      "{{#label}} with value {:[.]} fails to match the required pattern: example@example.com",
+  name: Joi.string().min(2).required(),
+  email: Joi.string().pattern(Regexps.EMAIL).required().messages({
+    'string.pattern.base':
+      '{{#label}} with value {:[.]} fails to match the required pattern: example@example.com',
   }),
   password: Joi.string().min(6).required(),
-  subscription: Joi.string().valid("starter", "pro", "business"),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required().messages({
-    "string.pattern.base":
-      "{{#label}} with value {:[.]} fails to match the required pattern: example@example.com",
+  email: Joi.string().pattern(Regexps.EMAIL).required().messages({
+    'string.pattern.base':
+      '{{#label}} with value {:[.]} fails to match the required pattern: example@example.com',
   }),
   password: Joi.string().min(6).required(),
 });
 
-const subscriptionSchema = Joi.object({
-  subscription: Joi.string().required().valid("starter", "pro", "business"),
-});
-
-module.exports = { registerSchema, loginSchema, subscriptionSchema };
+module.exports = { registerSchema, loginSchema };
