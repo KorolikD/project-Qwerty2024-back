dd160d541adf650dd9c9bf318a787420adf47322
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-03-01 00:32:44 +0200

    feat: add user logout controller

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,11 +1,8 @@
-const { User } = require("../../models");
+const { User } = require('../../models');
 
 const logout = async (req, res) => {
-  const { _id: id } = req.user;
-
-  await User.findByIdAndUpdate(id, { token: "" });
-
-  res.status(204).json({ message: "Logout success" });
+  await User.findByIdAndUpdate(req.user._id, { token: '' });
+  res.status(200).json({ message: 'Logout success' });
 };
 
 module.exports = logout;
