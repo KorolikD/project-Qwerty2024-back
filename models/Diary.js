010a71b66ea9995b416630da9747972d656e2881
Dmytro Korolik <dimytry.korolik@gmail.com>
2024-02-29 18:15:41 +0200

    commit

---
+++ models/Diary.js
@@ -0,0 +1,5 @@
+const { Schema, model } = require("mongoose");
+
+const Diary = new Schema({});
+
+module.exports = model("diary", Diary);
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const User = require("./User");
+const Diary = require("./Diary");
 
-module.exports = { User };
+module.exports = { User, Diary };
