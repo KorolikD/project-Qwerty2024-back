75c81ba96b7222afa7d95c69d5c8463d2530eae3
Anna Yavorska <aniayavorsk@gmail.com>
2024-03-01 17:33:09 +0100

    add Product model and logic to products

--- controllers/products/getAllCategories.js
+++ controllers/products/getAllCategories.js
@@ -1,5 +1,8 @@
 const productsCategories = require("../../data/");
+const data = require("../../data");
 
-const getAllCategories = async (req, res) => {};
+const getAllCategories = async (req, res) => {
+  res.json(data);
+};
 
 module.exports = getAllCategories;
--- controllers/products/getProducts.js
+++ controllers/products/getProducts.js
@@ -1,3 +1,19 @@
-const getProducts = async (req, res) => {};
+const Product = require("../../models/Product");
+
+const getProducts = async (req, res) => {
+  const { blood } = req.user;
+
+  const { category, title, allowed } = req.query;
+
+  const query = {};
+  category && (query.category = category);
+  title && (query.title = new RegExp(title, "i"));
+  allowed &&
+    (query[`groupBloodNotAllowed.${blood}`] = allowed.toLowerCase() === "true");
+
+  const productsList = await Product.find(query);
+
+  res.json(productsList);
+};
 
 module.exports = getProducts;
