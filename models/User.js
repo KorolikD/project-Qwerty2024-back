4d4c0a0e908949e86ef0bc595fd4da55a2950364
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-03-02 17:44:16 +0200

    feat: update user mongoose schema (add new params)

--- models/User.js
+++ models/User.js
@@ -1,31 +1,94 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const Regexps = { EMAIL: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ };
+const Enums = {
+  BLOOD: [1, 2, 3, 4],
+  SEX: ["male", "female"],
+  LEVEL_ACTIVITY: [1, 2, 3, 4, 5],
+};
 
 const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, 'Name is required'],
+      required: [true, "Name is required"],
       minlength: 2,
     },
     email: {
       type: String,
-      required: [true, 'Email is required'],
+      required: [true, "Email is required"],
       unique: true,
       match: Regexps.EMAIL,
     },
     password: {
       type: String,
-      required: [true, 'Set password for user'],
+      required: [true, "Set password for user"],
       minlength: 6,
     },
-
-    token: String,
+    token: {
+      type: String,
+      default: null,
+    },
+    avatarURL: {
+      type: String,
+      default:
+        "https://t4.ftcdn.net/jpg/06/35/83/71/360_F_635837151_QaS3vQk9cP4iWX1fTOXVac11kVWvfVDl.jpg",
+    },
+    height: {
+      type: Number,
+      default: null,
+      min: 150,
+    },
+    currentWeight: {
+      type: Number,
+      default: null,
+      min: 35,
+    },
+    desiredWeight: {
+      type: Number,
+      default: null,
+      min: 35,
+    },
+    birthday: {
+      type: Date,
+      default: null,
+      validate: {
+        validator: function (value) {
+          const birthdate = new Date(value);
+          const maxDate = new Date();
+          maxDate.setFullYear(maxDate.getFullYear() - 18);
+          return birthdate <= maxDate;
+        },
+        message: (props) => `${props.value} is not at least 18 years ago`,
+      },
+    },
+    blood: {
+      type: Number,
+      default: null,
+      enum: Enums.BLOOD,
+    },
+    sex: {
+      type: String,
+      default: null,
+      enum: Enums.SEX,
+    },
+    levelActivity: {
+      type: Number,
+      default: null,
+      enum: Enums.LEVEL_ACTIVITY,
+    },
+    bmr: {
+      type: Number,
+      default: null,
+    },
+    dpa: {
+      type: Number,
+      default: null,
+    },
   },
   { versionKey: false, timestamps: true }
 );
 
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;
