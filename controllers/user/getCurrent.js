f03e6796bd71546cee6cf520c6463e4ebe7f41ab
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-03-04 13:00:47 +0200

    refactor: add more user getCurrent controller

--- controllers/user/getCurrent.js
+++ controllers/user/getCurrent.js
@@ -1,6 +1,36 @@
 const getCurrent = async (req, res) => {
-  const { name, email } = req.user;
-  res.json({ name, email });
+  const {
+    name,
+    email,
+    _id,
+    avatarURL,
+    height,
+    currentWeight,
+    desiredWeight,
+    birthday,
+    blood,
+    sex,
+    levelActivity,
+    bmr,
+    dpa,
+    createdAt,
+  } = req.user;
+  res.json({
+    name,
+    email,
+    _id,
+    avatarURL,
+    height,
+    currentWeight,
+    desiredWeight,
+    birthday,
+    blood,
+    sex,
+    levelActivity,
+    bmr,
+    dpa,
+    createdAt,
+  });
 };
 
 module.exports = getCurrent;
