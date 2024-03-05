f3a0df1657f6726555bdbd7117a5353dd2486339
Anna Yavorska <aniayavorsk@gmail.com>
2024-03-05 14:45:47 +0100

    add second part of statistics

--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,14 @@ const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const getUserAge = require("./getUserAge");
 const filterExercises = require("./filterExercises");
+const aggregateArrayLength = require("./aggregateArrayLength");
+const aggregateSum = require("./aggregateSum");
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   getUserAge,
   filterExercises,
+  aggregateSum,
+  aggregateArrayLength,
 };
